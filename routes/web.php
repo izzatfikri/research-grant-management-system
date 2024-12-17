787dfbc36b4884b82573e38177de10149c195960
izzatfikri <izzatfikri.fikri@gmail.com>
2024-12-17 18:49:30 +0800

    Add resource routes for Academician, Grant, and Milestone controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AcademicianController;
+use App\Http\Controllers\GrantController;
+use App\Http\Controllers\MilestoneController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('academicians', AcademicianController::class);
+Route::resource('grants', GrantController::class);
+Route::resource('milestones', MilestoneController::class);
