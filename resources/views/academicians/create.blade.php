e6e8a6f93d892b6b4f292e42b3b8d98c162fe92a
izzatfikri <izzatfikri.fikri@gmail.com>
2024-12-19 12:11:57 +0800

    add dropdown selection for department, college and position

--- resources/views/academicians/create.blade.php
+++ resources/views/academicians/create.blade.php
@@ -31,15 +31,46 @@
                 </div>
                 <div class="form-group mb-3">
                     <label class="form-label" for="college">College</label>
-                    <input type="text" class="form-control" name="college" required>
+                    <select class="form-control" name="college" required>
+                        <option value="">Select College</option>
+                        <option value="College of Engineering (COE)">College of Engineering (COE)</option>
+                        <option value="UNITEN Business School (UBS)">UNITEN Business School (UBS)</option>
+                        <option value="College of Computing & Informatics (CCI)">College of Computing & Informatics (CCI)</option>
+                        <option value="College of Continuing Education (CCEd)">College of Continuing Education (CCEd)</option>
+                        <option value="College of Graduate Studies (COGS)">College of Graduate Studies (COGS)</option>
+                    </select>
                 </div>
                 <div class="form-group mb-3">
-                    <label class="form-label" for="department">Department</label>
-                    <input type="text" class="form-control" name="department" required>
+                <label class="form-label" for="department">Department</label>
+                <select class="form-control" name="department" id="department" required>
+                    <option value="">Select Department</option>
+                    <option value="Department of Civil Engineering (COE)">Department of Civil Engineering (COE)</option>
+                    <option value="Department of Electrical & Electronics Engineering (COE)">Department of Electrical & Electronics Engineering (COE)</option>
+                    <option value="Department of Mechanical Engineering">Department of Mechanical Engineering (COE)</option>
+                    <option value="Department of Engineering Foundation and General Studies (COE)">Department of Engineering Foundation and General Studies (COE)</option>
+                    <option value="Department of Accounting and Economic (UBS)">Department of Accounting and Economic (UBS)</option>
+                    <option value="Department of Accounting and Finance (UBS)">Department of Accounting and Finance (UBS)</option>
+                    <option value="Department of Management (UBS)">Department of Management (UBS)</option>
+                    <option value="Department of Business and Management (UBS)">Department of Business and Management (UBS)</option>
+                    <option value="Department of Computing (CCI)">Department of Computing (CCI)</option>
+                    <option value="Department of Informatics (CCI)">Department of Informatics (CCI)</option>
+                    <option value="Department of Computing Foundation and Diploma Studies (CCI)">Department of Computing Foundation and Diploma Studies (CCI)</option>
+                    <option value="Department of Social Sciences and Humanities (CCEd)">Department of Social Sciences and Humanities (CCEd)</option>
+                    <option value="Department of Languages and Communication (CCEd)">Department of Languages and Communication (CCEd)</option>
+                    <option value="Department of Lifelong Learning (CCEd)">Department of Lifelong Learning (CCEd)</option>
+                    <option value="Department of APEL (CCEd)">Department of APEL (CCEd)</option>
+                    <option value="Department of Graduate Studies (COGS)">Department of Graduate Studies (COGS)</option>
+                </select>
                 </div>
                 <div class="form-group mb-3">
                     <label class="form-label" for="position">Position</label>
-                    <input type="text" class="form-control" name="position" required>
+                    <select class="form-control" name="position" id="position" required>
+                        <option value="">Select Position</option>
+                        <option value="Professor">Professor</option>
+                        <option value="Assoc Prof.">Assoc Prof.</option>
+                        <option value="Senior Lecturer">Senior Lecturer</option>
+                        <option value="Lecturer">Lecturer</option>
+                    </select>
                 </div>
                 <button type="submit" class="btn btn-primary w-100">Create Academician</button>
             </form>
