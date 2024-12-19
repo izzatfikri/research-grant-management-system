8324cf15def1d58088ad743c15402ccb94e5fbd1
izzatfikri <izzatfikri.fikri@gmail.com>
2024-12-19 13:45:02 +0800

    modify the view for edit and show in academician

--- resources/views/academicians/edit.blade.php
+++ resources/views/academicians/edit.blade.php
@@ -1,4 +1,5 @@
-@extends('layouts.app')
+@extends('layouts.tabler-template')
+@section('title', 'Edit Academician')
 @section('content')
 <div class="container mt-5">
     <div class="card">
@@ -10,28 +11,59 @@
                 @method('PUT')
                 @csrf 
                 <div class="form-group mb-3">
-                    <label class="form-label">Name</label>
+                    <label class="form-label" for="name">Name</label>
                     <input type="text" class="form-control" name="name" value="{{ $academician->name }}">
                 </div>
                 <div class="form-group mb-3">
-                    <label class="form-label">Staff Number</label>
+                    <label class="form-label" for="staff_number">Staff Number</label>
                     <input type="text" class="form-control" name="staff_number" value="{{ $academician->staff_number }}">
                 </div>
                 <div class="form-group mb-3">
-                    <label class="form-label">Email</label>
+                    <label class="form-label" for="email">Email</label>
                     <input type="email" class="form-control" name="email" value="{{ $academician->email }}">
                 </div>
                 <div class="form-group mb-3">
-                    <label class="form-label">College</label>
-                    <input type="text" class="form-control" name="college" value="{{ $academician->college }}">
+                    <label class="form-label" for="college">College</label>
+                    <select class="form-control" name="college" required>
+                        <option value="{{ $academician->college }}" selected>{{ $academician->college }}</option>
+                        <option value="College of Engineering (COE)">College of Engineering (COE)</option>
+                        <option value="UNITEN Business School (UBS)">UNITEN Business School (UBS)</option>
+                        <option value="College of Computing & Informatics (CCI)">College of Computing & Informatics (CCI)</option>
+                        <option value="College of Continuing Education (CCEd)">College of Continuing Education (CCEd)</option>
+                        <option value="College of Graduate Studies (COGS)">College of Graduate Studies (COGS)</option>
+                    </select>
                 </div>
                 <div class="form-group mb-3">
-                    <label class="form-label">Department</label>
-                    <input type="text" class="form-control" name="department" value="{{ $academician->department }}">
+                    <label class="form-label" for="department">Department</label>
+                    <select class="form-control" name="department" required>
+                        <option value="{{ $academician->department }}" selected>{{ $academician->department }}</option>
+                        <option value="Department of Civil Engineering (COE)">Department of Civil Engineering (COE)</option>
+                        <option value="Department of Electrical & Electronics Engineering (COE)">Department of Electrical & Electronics Engineering (COE)</option>
+                        <option value="Department of Mechanical Engineering (COE)">Department of Mechanical Engineering (COE)</option>
+                        <option value="Department of Engineering Foundation and General Studies (COE)">Department of Engineering Foundation and General Studies (COE)</option>
+                        <option value="Department of Accounting and Economic (UBS)">Department of Accounting and Economic (UBS)</option>
+                        <option value="Department of Accounting and Finance (UBS)">Department of Accounting and Finance (UBS)</option>
+                        <option value="Department of Management (UBS)">Department of Management (UBS)</option>
+                        <option value="Department of Business and Management (UBS)">Department of Business and Management (UBS)</option>
+                        <option value="Department of Computing (CCI)">Department of Computing (CCI)</option>
+                        <option value="Department of Informatics (CCI)">Department of Informatics (CCI)</option>
+                        <option value="Department of Computing Foundation and Diploma Studies (CCI)">Department of Computing Foundation and Diploma Studies (CCI)</option>
+                        <option value="Department of Social Sciences and Humanities (CCEd)">Department of Social Sciences and Humanities (CCEd)</option>
+                        <option value="Department of Languages and Communication (CCEd)">Department of Languages and Communication (CCEd)</option>
+                        <option value="Department of Lifelong Learning (CCEd)">Department of Lifelong Learning (CCEd)</option>
+                        <option value="Department of APEL (CCEd)">Department of APEL (CCEd)</option>
+                        <option value="Department of Graduate Studies (COGS)">Department of Graduate Studies (COGS)</option>
+                    </select>
                 </div>
                 <div class="form-group mb-3">
-                    <label class="form-label">Position</label>
-                    <input type="text" class="form-control" name="position" value="{{ $academician->position }}">
+                    <label class="form-label" for="position">Position</label>
+                    <select class="form-control" name="position" required>
+                        <option value="{{ $academician->position }}" selected>{{ $academician->position }}</option>
+                        <option value="Professor">Professor</option>
+                        <option value="Assoc Prof.">Assoc Prof.</option>
+                        <option value="Senior Lecturer">Senior Lecturer</option>
+                        <option value="Lecturer">Lecturer</option>
+                    </select>
                 </div>
                 <button type="submit" class="btn btn-primary w-100">Save</button>
             </form>
--- resources/views/academicians/show.blade.php
+++ resources/views/academicians/show.blade.php
@@ -1,14 +1,144 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Academician Details</title>
-</head>
-<body>
-    <h1>{{ $academician->name }}</h1>
-    <p>Staff Number: {{ $academician->staff_number }}</p>
-    <p>Email: {{ $academician->email }}</p>
-    <p>College: {{ $academician->college }}</p>
-    <p>Department: {{ $academician->department }}</p>
-    <p>Position: {{ $academician->position }}</p>
-</body>
-</html>
+@extends('layouts.tabler-template')
+@section('content')
+<section style="background-color: #eee;">
+  <div class="container py-5">
+    <div class="row">
+      <div class="col">
+        <nav aria-label="breadcrumb" class="bg-body-tertiary rounded-3 p-3 mb-4">
+          <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Academicians</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Academician Profile</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+    <div class="row">
+        <div class="col">
+        <a href="{{ route('academicians.index') }}" class="btn btn-secondary mt-3">Back to Academicians</a>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="card mb-4">
+          <div class="card-body text-center">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
+              class="rounded-circle img-fluid" style="width: 150px;">
+            <h5 class="my-3">{{ $academician->name }}</h5>
+            <p class="text-muted mb-1">{{ $academician->position }}</p>
+            <p class="text-muted mb-4">{{ $academician->college }}</p>
+            <!--<div class="d-flex justify-content-center mb-2">
+              <button type="button" class="btn btn-primary">Follow</button>
+              <button type="button" class="btn btn-outline-primary ms-1">Message</button>
+            </div>-->
+          </div>
+        </div>
+        <div class="card mb-4 mb-lg-0">
+          <div class="card-body p-0">
+            <ul class="list-group list-group-flush rounded-3">
+              <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <i class="fas fa-envelope fa-lg text-warning"></i>
+                <p class="mb-0">{{ $academician->email }}</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <i class="fas fa-building fa-lg text-body"></i>
+                <p class="mb-0">{{ $academician->department }}</p>
+              </li>
+              <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <i class="fas fa-university fa-lg" style="color: #55acee;"></i>
+                <p class="mb-0">{{ $academician->university }}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-8">
+        <div class="card mb-4">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Full Name</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ $academician->name }}</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Staff Number</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ $academician->staff_number }}</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ $academician->email }}</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">College</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ $academician->college }}</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Department</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ $academician->department }}</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Position</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">{{ $academician->position }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!--
+        <h4 class="profile-title">About Me</h4>
+        <div class="card mb-4 shadow-sm animate__animated animate__fadeIn">
+            <div class="card-body">
+                <p>{{ $academician->about }}</p>
+            </div>
+        </div>-->
+
+        <!--
+        <h4 class="profile-title">Working Experiences</h4>
+        <table class="table table-bordered profile-table">
+            <tr>
+                <th>Position</th>
+                <th>Place of Work</th>
+                <th>Start</th>
+                <th>End</th>
+            </tr>
+
+        </table>-->
+
+       
+      </div>
+    </div>
+  </div>
+</section>
+@endsection
+
+@push('headerScripts')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+@endpush
